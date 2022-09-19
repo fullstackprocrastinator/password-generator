efed475c77e8e38c2a4ec3c9a323e82d44a88f84
fullstackprocrastinator <111703058+fullstackprocrastinator@users.noreply.github.com>
2022-09-19 18:53:37 +0100

    files uploaded

---
+++ scripts.js
@@ -0,0 +1,115 @@
+const characters = [
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z",
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+  "0",
+  "1",
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "~",
+  "`",
+  "!",
+  "@",
+  "#",
+  "$",
+  "%",
+  "^",
+  "&",
+  "*",
+  "(",
+  ")",
+  "_",
+  "-",
+  "+",
+  "=",
+  "{",
+  "[",
+  "}",
+  "]",
+  ",",
+  "|",
+  ":",
+  ";",
+  "<",
+  ">",
+  ".",
+  "?",
+  "/",
+];
+
+let passwordLength = 15;
+let passOneField = document.getElementById("passwords-sect1");
+let passTwoField = document.getElementById("passwords-sect2");
+let passwordOne = "";
+let passwordTwo = "";
+
+function generatePassword() {
+  let password = "";
+  for (let i = 0; i < passwordLength; i++) {
+    let randomInt = Math.floor(Math.random() * characters.length);
+    password += characters[randomInt];
+  }
+  return password;
+}
+
+function passGen() {
+  passwordOne = generatePassword();
+  passwordTwo = generatePassword();
+  passOneField.textContent = passwordOne;
+  passTwoField.textContent = passwordTwo;
+}
